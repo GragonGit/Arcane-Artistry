225c30ee358f49fb65a95a6d3e16ee1251b0effc
TanConan <46909538+TanConan@users.noreply.github.com>
2026-09-10 22:05:05 +0200

    Initial fabric files

---
+++ src/client/java/gragongit/arcaneartistry/client/ArcaneArtistryDataGenerator.java
@@ -0,0 +1,11 @@
+package gragongit.arcaneartistry.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArcaneArtistryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
