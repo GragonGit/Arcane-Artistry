51c758784719b104d6450ea46e8a55b492da8873
TanConan <46909538+TanConan@users.noreply.github.com>
2026-09-11 00:18:49 +0200

    Multi sourceSet setup

---
+++ src/elements/java/gragongit/arcaneartistry/elements/common/ArcaneArtistryElements.java
@@ -0,0 +1,22 @@
+package gragongit.arcaneartistry.elements.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.Identifier;
+
+public class ArcaneArtistryElements implements ModInitializer {
+  public static final String MOD_ID = "arcane-artistry-elements";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Arcane Artistry Elements!");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
