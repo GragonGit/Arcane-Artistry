37843ad064a97c18e5cd8395010e9874229867b5
Gragon <gragonator02@gmail.com>
2026-09-12 03:04:35 +0200

    Added Registry Utils

---
+++ src/main/java/gragongit/arcaneartistry/common/registry/RegistryUtils.java
@@ -0,0 +1,19 @@
+package gragongit.arcaneartistry.common.registry;
+
+import java.util.Optional;
+import gragongit.arcaneartistry.common.staff.StaffDefinition;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+
+public final class RegistryUtils {
+  private RegistryUtils() {}
+
+  public static Optional<StaffDefinition> tryGetStaff(Level level, Item item) {
+    return Optional.ofNullable(level.registryAccess().lookupOrThrow(ModRegistries.STAFF_KEY).getValue(BuiltInRegistries.ITEM.getKey(item)));
+  }
+
+  public static boolean isStaff(Level level, Item item) {
+    return tryGetStaff(level, item).isPresent();
+  }
+}
