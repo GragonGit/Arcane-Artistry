1d4c214497d22c88ce23148d3da6fbab1448e0df
TanConan <46909538+TanConan@users.noreply.github.com>
2026-09-16 01:17:30 +0200

    Move CLIENT Mixins to CLIENT Mixin json

--- src/main/java/gragongit/arcaneartistry/client/mixin/CastingMouseDeltaMixin.java
+++ src/main/java/gragongit/arcaneartistry/client/mixin/CastingMouseDeltaMixin.java
@@ -1,4 +1,4 @@
-package gragongit.arcaneartistry.common.mixin;
+package gragongit.arcaneartistry.client.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
